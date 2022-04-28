26b1a58288c4f12dfa2034cf77a1aec2ff280d39
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-04-28 17:42:50 -0500

    completed idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -15,3 +15,59 @@ request.onupgradeneeded = function(event) {
       uploadPizza();
     }
   };
+
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+
+  function saveRecord(record) {
+    const transaction = db.transaction(['pending'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('pending');
+  
+    // add record to your store with add method.
+    budgetObjectStore.add(record);
+  }
+
+  function uploadBudget() {
+    
+    const transaction = db.transaction(['pending'], 'readwrite');
+  
+    const pizzaObjectStore = transaction.objectStore('pending');
+  
+    const getAll = budgetObjectStore.getAll();
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+    
+              const transaction = db.transaction(['pending'], 'readwrite');
+              const pizzaObjectStore = transaction.objectStore('pending');
+              // clear all items in your store
+              budgetObjectStore.clear();
+            })
+            .catch(err => {
+              // set reference to redirect back here
+              console.log(err);
+            });
+        }
+      };
+  }
+
+  window.addEventListener('online', uploadBudget);
+
+
+  
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,4 @@
 // add event AudioListener, casche, etc
 
+
+
