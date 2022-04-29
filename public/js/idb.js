47f2ce148da1a1ff9887f5c26f9f9a48d9c89851
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-04-28 20:17:54 -0500

    ldb modification

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,5 @@
 let db;
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open('budget-tracker', 1);
 
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
@@ -25,8 +25,8 @@ function saveRecord(record) {
 
     const budgetObjectStore = transaction.objectStore('pending');
 
-
     budgetObjectStore.add(record);
+    alert('budget has ben updated!');
 }
 
 function uploadBudget() {
@@ -43,9 +43,8 @@ function uploadBudget() {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
+                    'Content-Type': 'application/json',
+                },
             })
                 .then(response => response.json())
                 .then(serverResponse => {
